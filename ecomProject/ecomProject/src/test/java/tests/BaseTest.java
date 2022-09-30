a58a4f50adbc33b83f7e32e08f89cc7eedee43ce
AkshitKaushik1 <109943169+AkshitKaushik1@users.noreply.github.com>
2022-09-30 14:58:47 +0530

    Add files via upload

---
+++ ecomProject/ecomProject/src/test/java/tests/BaseTest.java
@@ -0,0 +1,55 @@
+
+package tests;
+
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import java.time.Duration;
+
+import pages.AddNewAccountPage;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.MyAccountPage;
+import util.Log;
+
+public class BaseTest {
+    public WebDriver driver;
+    public LoginPage  loginPage;
+    public HomePage homePage;
+    public MyAccountPage myaccountPage;
+    public AddNewAccountPage addnewAccountPage;
+    public WebDriverWait wait;
+    public WebDriver getDriver() {
+        return driver;
+    }
+    @BeforeClass
+    public void classLevelSetup() {
+        Log.info("Tests is starting!");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\akshi\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+        ChromeOptions option=new ChromeOptions();
+        option.setPageLoadStrategy(PageLoadStrategy.NONE);
+        option.addArguments("--disable-notifications");
+        driver = new ChromeDriver(option);
+        driver.manage().window().maximize();
+        
+    }
+    @BeforeMethod
+    public void methodLevelSetup() {
+        loginPage = new LoginPage(driver);
+        homePage=new HomePage(driver);
+        myaccountPage=new MyAccountPage(driver);
+        addnewAccountPage = new AddNewAccountPage(driver);
+        
+        wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+       }
+    @AfterClass
+    public void teardown() {
+        Log.info("Tests are ending!");
+      // driver.quit();
+    }
+}
